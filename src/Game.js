5a9b6a21897883548ece27a2f71648724fed35bc
MatGralak <mateuszgralak89@gmail.com>
2019-10-07 20:09:50 +0200

    initial commit

---
+++ src/Game.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import Number from './Number';
+import sampleSize from 'lodash.samplesize';
+
+const randomNumberBetween = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+class Game extends React.Component {
+    static bgColors = {
+        playing: '#ccc',
+        won: 'green',
+        lost: 'red',
+      };
+      
+    state = {  
+        gameStatus: 'new',
+        remainingSeconds: this.props.initialSeconds,  
+        selectedIds: [],
+    };   
+    challengeNumbers = Array
+      .from({ length: this.props.challengeSize })
+      .map(() => randomNumberBetween(...this.props.challengeRange));
+
+    isNumberAvailable = (numberIndex) => 
+      this.state.selectedIds.indexOf(numberIndex) === -1;
+
+    target = sampleSize(
+        this.challengeNumbers,
+        this.props.challengeSize - 2
+      ).reduce((acc, curr) => acc + curr, 0);
+
+    componentWillUnmount() {
+      clearInterval(this.intervalId);
+    }
+
+    componentDidMount() {
+      if (this.props.autoPlay) {
+        this.startGame();
+      }
+    }
+
+    startGame = () => {
+      this.setState({ gameStatus: 'playing' }, () => {
+        this.intervalId = setInterval(() => {
+          this.setState((prevState) => {
+            const newRemainingSeconds = prevState.remainingSeconds - 1;
+            if (newRemainingSeconds === 0) {
+              clearInterval(this.intervalId);
+              return { gameStatus: 'lost', remainingSeconds: 0 };
+            }
+            return { remainingSeconds: newRemainingSeconds };
+          });
+        }, 1000);
+      });
+    };
+
+    selectNumber = (numberIndex) => {
+      if (this.state.gameStatus !== 'playing') {
+        return;
+      }
+      this.setState(
+        (prevState) => ({
+          selectedIds: [...prevState.selectedIds, numberIndex],
+          gameStatus: this.calcGameStatus([
+            ...prevState.selectedIds,
+            numberIndex,
+          ]),
+        }),
+        () => {
+          if (this.state.gameStatus !== 'playing') {
+            clearInterval(this.intervalId);
+          }
+        }
+      );
+    };
+
+    calcGameStatus = (selectedIds) => {
+      const sumSelected = selectedIds.reduce(
+        (acc, curr) => acc + this.challengeNumbers[curr],0);
+        if (sumSelected < this.target) {
+          return 'playing';
+        }  
+      return sumSelected === this.target ? 'won' : 'lost';
+    };
+
+    render() {
+      const { gameStatus, remainingSeconds } = this.state;
+      return (
+        <div className="game">
+          <div className="help">
+            Pick 4 numbers that sum to the target in 15 seconds
+          </div>
+          <div 
+            className="target"
+            style={{backgroundColor: Game.bgColors[gameStatus] }}
+          >
+            {this.state.gameStatus === 'new' ? '?' : this.target}
+          </div>
+          <div className="challenge-numbers">
+            {this.challengeNumbers.map((value, index) =>
+                (<Number 
+                key={index}
+                id={index}
+                value={this.state.gameStatus === 'new' ? '?' : value} 
+                clickable={this.isNumberAvailable(index)}
+                onClick={this.selectNumber}
+                />)
+              )}
+          </div>
+          <div className="footer">
+            {this.state.gameStatus === 'new' ? (
+              <button onClick={this.startGame}>Start</button>
+              ) : (
+                <div className="timer-value">{remainingSeconds}</div>
+              )}  
+            {['won', 'lost'].includes(this.state.gameStatus
+              ) && (
+              <button onClick={this.props.onPlayAgain}>Play Again</button>
+              )}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+export default Game;
